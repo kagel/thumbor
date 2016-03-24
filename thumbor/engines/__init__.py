d105b42d314edd93536506eb8850a944658be773
Dmitriy Khvatov <kagel@users.noreply.github.com>
2016-03-24 16:14:08 +0300

    Temporary fix for the EXIF issue

--- thumbor/engines/__init__.py
+++ thumbor/engines/__init__.py
@@ -257,9 +257,11 @@ class BaseEngine(object):
 
         if orientation != 1 and override_exif:
             segment = self._get_exif_segment()
-            if segment:
-                segment.primary['Orientation'] = [1]
-                self.exif = segment.get_data()
+                try:
+                    segment.primary['Orientation'] = [1]
+                    self.exif = segment.get_data()
+                except:
+                    pass
 
     def gen_image(self):
         raise NotImplementedError()
